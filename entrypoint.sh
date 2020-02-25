80eac479c42edf1dc8a38ebd632628f4863c6f90
Carlos De Niz <Carlos_DeNiz@hms.harvard.edu>
2020-02-25 17:42:54 -0500

    Updated steps for entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # strip out annotations and stuff
-
 # strip FORMAT down to GT only writing to /opt/local/hpds/vcfInput/input.vcf
-
-mkdir -p /opt/local/hpds/vcfInput
-
 # create vcfIndex.tsv in /opt/local/hpds pointing to /opt/local/hpds/vcfInput/input.vcf
-
 # run HPDS variant loader
 # if needed export HEAPSIZE=XXXXXX where XXXXXX is the number of MB of RAM to make available
-cd /
-java -Xmx${HEAPSIZE:-2048}m -jar VCFLocalLoader-jar-with-dependencies.jar 
 
-# compress output of HPDS loader
+# This Python script addesses all the above
+python3 runHPDS.py --input myfile.vcf.gz --mapping myMappingFile.csv
+
+# Run Docker composer 
+docker-compose -f $HOME/pic-sure-hpds/docker/pic-sure-hpds-etl/docker-compose-variant-loader.yml up -d
+
+# Compress output of HPDS loader
 cd /opt/local/hpds/all
-tar -xvzf /hpds_variant_data.tar.gz *
+tar -cvzf /hpds_variant_data.tar.gz *
 
 
